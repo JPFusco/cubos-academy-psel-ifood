f2353a6f1daf656e7dee565cfb77f7fe9e766576
JPFusco <85804722+JPFusco@users.noreply.github.com>
2021-08-10 19:02:39 -0300

    Versão Final

---
+++ esconde-esconde.js
@@ -0,0 +1,10 @@
+function solucao(numero) {
+	if (numero > 20){
+        for (let i = 21; i<=numero; i++){
+            console.log(i)
+        }
+    } else {
+        console.log("VOCE CONSEGUE, MANINHO")
+    }
+    
+}
